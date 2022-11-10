3c4eb03992b35c31430c76b941273a4c268e689a
Huskies Prog <57733100+HuskiesProg@users.noreply.github.com>
2022-11-09 20:00:38 -0500

    Manque La Base Pilotable a Tout Recomencer

--- src/main/java/frc/robot/commands/Pousser.java
+++ src/main/java/frc/robot/commands/Pousser.java
@@ -4,28 +4,22 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Poussoir;
 
-/** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+public class Pousser extends CommandBase {
+  Poussoir piston;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+  public Pousser(Poussoir piston) {
+    this.piston = piston;
+    addRequirements(piston);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    piston.pistonOut();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -33,7 +27,9 @@ public class ExampleCommand extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    piston.pistonIn();
+  }
 
   // Returns true when the command should end.
   @Override
---
+++ src/main/java/frc/robot/subsystems/Poussoir.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Poussoir extends SubsystemBase {
+  private DoubleSolenoid piston = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+  
+  public Poussoir() {
+    pistonIn();
+  }
+
+  @Override
+  public void periodic() {}
+
+  public void pistonIn() {
+    piston.set(Value.kForward);
+  }
+
+  public void pistonOut() {
+    piston.set(Value.kReverse);
+  }
+
+}
