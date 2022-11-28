f851664e8e9648f0b244e27b9662f93d15d65071
Huskies Prog <huskies5439@polych.ca>
2022-11-28 13:42:19 -0500

    ajustement autonomes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,7 +7,7 @@ package frc.robot;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 
 public final class Constants { //TO DO changer les données
-    public static final DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(0.63); 
+    public static final DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(0); 
     public static final double kVRamsete = 4.48;
     public static final double kSRamsete = 0.6;
     public static final double kPRamsete = 2;
